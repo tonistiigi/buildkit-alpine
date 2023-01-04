419f2f5ca60ec6ac24cdb698d02cd14ee09e7275
Tonis Tiigi <tonistiigi@gmail.com>
2023-01-04 12:17:09 -0800

    parse yaml and pass to build func
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>

--- build.go
+++ build.go
@@ -2,18 +2,174 @@ package alpine
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
 
+	apko "chainguard.dev/apko/pkg/build/types"
+	"github.com/containerd/containerd/platforms"
 	"github.com/moby/buildkit/client/llb"
+	"github.com/moby/buildkit/exporter/containerimage/exptypes"
 	"github.com/moby/buildkit/frontend/gateway/client"
+	ocispecs "github.com/opencontainers/image-spec/specs-go/v1"
+	"github.com/pkg/errors"
+	"golang.org/x/sync/errgroup"
+	"gopkg.in/yaml.v3"
 )
 
+const (
+	defaultFilename = "Dockerfile"
+
+	keyFilename      = "filename"
+	keyPlatform      = "platform"
+	localContextName = "context"
+)
+
+var platformMapping = map[string]string{
+	"amd64": "x86_64",
+	"arm64": "aarch64",
+	"386":   "x86",
+	"arm":   "armv7l",
+}
+
+func alpinePlatform(p ocispecs.Platform) string {
+	v, ok := platformMapping[p.Architecture]
+	if !ok {
+		return p.Architecture
+	}
+	return v
+}
+
+func parsePlatforms(v string) ([]ocispecs.Platform, error) {
+	pl := []ocispecs.Platform{}
+	for _, s := range strings.Split(v, ",") {
+		s = strings.TrimSpace(s)
+		if s == "" {
+			continue
+		}
+		p, err := platforms.Parse(s)
+		if err != nil {
+			return nil, err
+		}
+		p = platforms.Normalize(p)
+		pl = append(pl, p)
+	}
+	return pl, nil
+}
+
 func Build(ctx context.Context, c client.Client) (*client.Result, error) {
-	def, err := llb.Image("alpine:latest").Marshal(ctx)
+	opts := c.BuildOpts()
+
+	fn := defaultFilename
+	if f, ok := opts.Opts[keyFilename]; ok {
+		fn = f
+	}
+
+	var pl []ocispecs.Platform
+	if v, ok := opts.Opts[keyPlatform]; ok {
+		var err error
+		pl, err = parsePlatforms(v)
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to parse platforms: %s", v)
+		}
+	}
+	if len(pl) == 0 {
+		pl = append(pl, platforms.Normalize(platforms.DefaultSpec()))
+	}
+
+	// TODO: git/http contexts
+	src := llb.Local(localContextName, llb.SessionID(c.BuildOpts().SessionID), llb.SharedKeyHint("alpine-filename"), llb.FollowPaths([]string{fn}))
+	def, err := src.Marshal(ctx)
 	if err != nil {
 		return nil, err
 	}
+
 	res, err := c.Solve(ctx, client.SolveRequest{
 		Definition: def.ToPB(),
 	})
+	if err != nil {
+		return nil, err
+	}
+	dt, err := res.Ref.ReadFile(ctx, client.ReadRequest{
+		Filename: fn,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	ic, err := parse(dt)
+	if err != nil {
+		return nil, err
+	}
+
+	res = client.NewResult()
+	expPlatforms := &exptypes.Platforms{
+		Platforms: make([]exptypes.Platform, len(pl)),
+	}
+
+	eg, ctx := errgroup.WithContext(ctx)
+	for k, p := range pl {
+		k, p := k, p
+		eg.Go(func() error {
+			r, err := buildPlatform(ctx, c, p, ic)
+			if err != nil {
+				return err
+			}
+			pkey := platforms.Format(p)
+			res.AddRef(pkey, r.Ref)
+			expPlatforms.Platforms[k] = exptypes.Platform{
+				ID:       pkey,
+				Platform: p,
+			}
+			return nil
+		})
+	}
+	if err := eg.Wait(); err != nil {
+		return nil, err
+	}
+
+	dt, err = json.Marshal(expPlatforms)
+	if err != nil {
+		return nil, err
+	}
+	res.AddMeta(exptypes.ExporterPlatformsKey, dt)
+
 	return res, err
+}
+
+func isIgnoreCache(c client.Client) bool {
+	if _, ok := c.BuildOpts().Opts["no-cache"]; ok {
+		return true
+	}
+	return false
+}
+
+func buildPlatform(ctx context.Context, c client.Client, p ocispecs.Platform, ic *apko.ImageConfiguration) (*client.Result, error) {
+	cmd := fmt.Sprintf(`sh -c "apk add --initdb --arch %s --root /out"`, alpinePlatform(p))
+
+	ro := []llb.RunOption{llb.Shlex(cmd), llb.Network(llb.NetModeNone)}
+	if isIgnoreCache(c) {
+		ro = append(ro, llb.IgnoreCache)
+	}
+	st := llb.Image("alpine").Run(ro...)
+	def, err := st.Marshal(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return c.Solve(ctx, client.SolveRequest{
+		Definition: def.ToPB(),
+	})
+}
+
+func parse(dt []byte) (*apko.ImageConfiguration, error) {
+	// TODO: apko doesn't have a clean types pkg. Upstream changes or copy/paste only the definitions
+	var ic apko.ImageConfiguration
+	if err := yaml.Unmarshal(dt, &ic); err != nil {
+		return nil, errors.Errorf("failed to parse image configuration: %w", err)
+	}
+	if err := ic.Validate(); err != nil {
+		return nil, err
+	}
+	return &ic, nil
 }
--- go.mod
+++ go.mod
@@ -3,33 +3,54 @@ module github.com/tonistiigi/buildkit-alpine
 go 1.18
 
 require (
+	chainguard.dev/apko v0.6.0
+	github.com/containerd/containerd v1.6.13
 	github.com/moby/buildkit v0.11.0-rc3
+	github.com/opencontainers/image-spec v1.1.0-rc2
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.9.0
+	golang.org/x/sync v0.1.0
+	gopkg.in/yaml.v3 v3.0.1
+
 )
 
 require (
-	github.com/Microsoft/go-winio v0.5.2 // indirect
-	github.com/containerd/containerd v1.6.13 // indirect
+	github.com/Microsoft/go-winio v0.6.0 // indirect
+	github.com/ProtonMail/go-crypto v0.0.0-20220824120805-4b6e5c587895 // indirect
+	github.com/acomagu/bufpipe v1.0.3 // indirect
+	github.com/cloudflare/circl v1.2.0 // indirect
 	github.com/containerd/continuity v0.3.0 // indirect
 	github.com/containerd/typeurl v1.0.2 // indirect
 	github.com/docker/distribution v2.8.1+incompatible // indirect
+	github.com/emirpasic/gods v1.18.1 // indirect
+	github.com/go-git/gcfg v1.5.0 // indirect
+	github.com/go-git/go-billy/v5 v5.3.1 // indirect
+	github.com/go-git/go-git/v5 v5.4.2 // indirect
 	github.com/gogo/googleapis v1.4.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-containerregistry v0.12.1 // indirect
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
+	github.com/imdario/mergo v0.3.13 // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
+	github.com/jinzhu/copier v0.3.5 // indirect
+	github.com/kevinburke/ssh_config v1.2.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/moby/sys/signal v0.7.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
-	github.com/opencontainers/image-spec v1.0.3-0.20220303224323-02efb9a75ee1 // indirect
+	github.com/sergi/go-diff v1.2.0 // indirect
 	github.com/tonistiigi/fsutil v0.0.0-20221114235510-0127568185cf // indirect
+	github.com/xanzy/ssh-agent v0.3.2 // indirect
 	go.opentelemetry.io/otel v1.4.1 // indirect
 	go.opentelemetry.io/otel/trace v1.4.1 // indirect
 	golang.org/x/crypto v0.2.0 // indirect
+	golang.org/x/mod v0.6.0 // indirect
 	golang.org/x/net v0.4.0 // indirect
-	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.3.0 // indirect
 	golang.org/x/text v0.5.0 // indirect
-	google.golang.org/genproto v0.0.0-20220706185917-7780775163c4 // indirect
+	golang.org/x/tools v0.1.12 // indirect
+	google.golang.org/genproto v0.0.0-20221010155953-15ba04fc1c0e // indirect
 	google.golang.org/grpc v1.50.1 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
 )

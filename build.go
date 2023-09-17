5e7081aa2032b34488a647a751b652618a0ed15c
Tonis Tiigi <tonistiigi@gmail.com>
2023-09-16 23:16:00 -0700

    fix dockerfile local name
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>

--- build.go
+++ build.go
@@ -24,7 +24,7 @@ const (
 
 	keyFilename      = "filename"
 	keyPlatform      = "platform"
-	localContextName = "context"
+	localContextName = "dockerfile"
 )
 
 var platformMapping = map[string]string{
